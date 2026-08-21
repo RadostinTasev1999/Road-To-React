35f627c506e339a3186a6ca839dfa38c5f3accbb
RadostinTasev1999 <radostintasev22@yahoo.com>
2026-08-21 18:44:09 +0300

    updated eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
+export default defineConfig(
+  [
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
@@ -18,4 +19,9 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+  {
+    rules: {
+      'react/prop-types': 'off'
+    }
+  }
 ])
